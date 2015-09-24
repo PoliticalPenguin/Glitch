cbbab4ef9c47bfdb9cea9c474039496ec82c395d
Hack Reactor Students <accounts@hackreactor.com>
2015-09-24 16:43:00 -0700

    Implement server-client play song integration

--- Client/YouTube/youtube.js
+++ Client/YouTube/youtube.js
@@ -4,25 +4,12 @@ angular.module('glitch.youtube', [
   ])
 .controller('youtubeController', function($scope) {
   $scope.pastVideos = [
-    'Test1',
-    'Test2',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
-    'Test',
   ]
 
   // Stores the currently playing video
   $scope.currentVideo = {  
-    url: 'https://www.youtube.com/watch?v=3PEGDGxZdzA#t=300s',
-    title: "Emancipator - Anthem (2006)",
-    duration: 60000,
+    url: 'https://www.youtube.com/watch?v=8tPnX7OPo0Q',
+    title: "Waiting For Server...",
   };
   
   // Stores the settings for the youtube-video element
@@ -32,9 +19,13 @@ angular.module('glitch.youtube', [
   }
 
   // Listen for a broadcast from the server
-  // socket.on('playVideo', function(data) {
-  //   // ALSO: we are going to push to past videos the current video 
-  //   // also also: add the timestart to the url, if necessary 
-  //   // $scope.currentVideo = PARSED FROM DATA
-  // })
+  socket.on('play', function(data) {
+    console.log(data);
+     // ALSO: we are going to push to past videos the current video 
+     // also also: add the timestart to the url, if necessary 
+     // $scope.currentVideo = PARSED FROM DATA
+    $scope.currentVideo.url = data.url + "#t=" + (data.time / 1000) +"s";
+    $scope.currentVideo.title = data.url;
+    $scope.pastVideos.push(data.url);
+  })
 })

5587dedf50b6353838076fd8b0b4af69f1e0a1bf
Nate Willard <nate.a.willard@gmail.com>
2015-09-28 20:12:10 -0700

    Filled out client-side test shells

--- test/client/angular.js
+++ test/client/angular.js
@@ -6,11 +6,17 @@ describe('Client-side Angular', function() {
   beforeEach(module('glitch'));
 
   describe('Video Display', function() {
+    it('should display videos', function() {
+    });
   });
   describe('Song History', function() {
-    it('should add songs to the song history', function() {
+    it('should add videos to the playlist', function() {
+    });
+    it('should remove videos once the playlist is full', function() {
     });
   });
   describe('Chat Messages', function() {
+    it('should add messages to the chat history', function() {
+    });
   });
 });

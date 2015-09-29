5282dd1bb444b5fd39f22c1023a7889954d15bda
Nate Willard <nate.a.willard@gmail.com>
2015-09-29 13:17:37 -0700

    Add basic client-side tests for angular youtube controller

--- test/client/angular.js
+++ test/client/angular.js
@@ -5,18 +5,49 @@ var expect = chai.expect;
 describe('Client-side Angular', function() {
   beforeEach(module('glitch'));
 
-  describe('Video Display', function() {
-    it('should display videos', function() {
+  var $controller;
+  var socketController;
+  var notify;
+  var socketFactory;
+
+  beforeEach(inject(function(_$controller_, _socket_) {
+    $controller = _$controller_;
+    notify = _socket_;
+  }));
+
+  describe('YouTube Handling', function() {
+    var $scope;
+    var controller;
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('youtubeController', {$scope: $scope});
+    });
+
+    describe('Video Display', function() {
+      it('should have a placeholder initial video', function() {
+        expect($scope.currentVideo).to.exist;
+        expect($scope.currentVideo.title).to.equal('Waiting For Server...');
+      });
+      it('should display videos', function(done) {
+        setTimeout(function() {
+          expect($scope.currentVideo.title).to.not.equal('Waiting For Server...');
+          done();
+        }, 1000);
+      });
+    });
+    describe('Song History', function() {
+      it('Should have an empty initial playlist', function() {
+        expect($scope.pastVideos).to.have.length(0);
+      });
+      it('should add videos to the playlist', function(done) {
+        setTimeout(function() {
+          console.log($scope);
+          console.log($scope.pastVideos);
+          done();
+        }, 1000);
+      });
     });
   });
-  describe('Song History', function() {
-    it('should add videos to the playlist', function() {
-    });
-    it('should remove videos once the playlist is full', function() {
-    });
-  });
-  describe('Chat Messages', function() {
-    it('should add messages to the chat history', function() {
-    });
+  describe('Chat Message Handling', function() {
   });
 });

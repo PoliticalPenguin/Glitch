b6b5493486273d5b23fef7ac41d35c13d6e69c64
Anton Solomonik <xerxesv@gmail.com>
2015-09-30 16:24:01 +0100

    Add addToPlaylist function that calls youTube search API

--- server/server.js
+++ server/server.js
@@ -36,6 +36,32 @@ fs.readFile(__dirname + '/playlist.json', function read(err, data) {
     runServer(playlist);
 });
 
+
+var addToPlayList = function (queryString) {
+  // Fetches only the IDs of the videos we are searching for
+  
+  var requestString = 'https://www.googleapis.com/youtube/v3/search?part=id&fields=items/id/videoId&type=video&videoEmbeddable=true&videoDuration=short&maxResults=5&q='+ queryString + '&key=' + youtubeKey;
+  https.get(requestString, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      var object = JSON.parse(body);
+      // Returns an array of video IDs and URLs as our playlist
+      var playlist = object.items.map(function(item) {
+        return 'https://www.youtube.com/watch?v=' + item.id.videoId;
+      });
+      console.log('the playlist: ', playlist);
+
+      
+    });
+  }).on('error', function(err) {
+    
+  }); 
+ 
+};
+
 var currentSong = module.exports.currentSong = {startMoment: null, endMoment: null, title: null};
 
 var runServer = function(playlist) {

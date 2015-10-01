3c8a12b5a39d88da3fd85338e4cba2abd2b1c1b6
Nate Willard <nate.a.willard@gmail.com>
2015-10-01 12:31:36 -0700

    Rename fetchPlaylist to fetchYoutubeResults

--- server/app/youtubeUtilities.js
+++ server/app/youtubeUtilities.js
@@ -1,6 +1,6 @@
 var https = require('https');
 var app = require(__dirname+'/../server.js');
-module.exports.fetchPlaylist = function (queryString, callback) {
+module.exports.fetchYoutubeResults = function (queryString, callback) {
   // Fetches only the IDs of the videos we are searching for
   var requestString = 'https://www.googleapis.com/youtube/v3/search?part=id&fields=items/id/videoId&type=video&videoEmbeddable=true&videoDuration=short&maxResults='+app.youtubeResults+'&q='+ queryString + '&key=' + app.youtubeKey;
   https.get(requestString, function(res) {

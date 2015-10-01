6860b6441807e4764e62cd9425bf41f158257197
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-01 11:12:14 -0700

    Refactor serverTest.js to start and stop songs with each test; refactor Gruntfile.js to mute non-critical error message relating to killing of node processes at end of tests

--- test/server/serverTest.js
+++ test/server/serverTest.js
@@ -9,21 +9,24 @@ var socket;
 describe('glitch Server Integration Tests', function () {
   this.timeout(5000); //Increased time before which a 'timeout' is assumed, due to the Youtube API occasionally taking over 2 seconds to serve a request.
 
-  beforeEach(function (done) {
+  beforeEach(function () {
     var options = {
       transports: ['websocket'],
       'force new connection': true
     };
     socket = io.connect("http://localhost:" + socketPort, options);
+    app.queueSong('https://www.youtube.com/watch?v=3PEGDGxZdzA');
+  });
 
-    done();
+  afterEach(function () {
+    app.setTimeLeft(0);
+    socket.disconnect();
   });
 
 
   it('accepts Socket.io connections on port ' + socketPort, function (done) {
     socket.on('connect', function (data) {
-      socket.disconnect();
-    done();
+      done();
     });
   });
 
@@ -38,7 +41,6 @@ describe('glitch Server Integration Tests', function () {
       socket2.on('connect', function (data2) {
         var connectionInfo2 = app.getConnectionInfo();
         expect(connectionInfo2.numClients).to.equal(2);
-        socket.disconnect();
         socket2.disconnect();
         done();
       });
@@ -46,8 +48,6 @@ describe('glitch Server Integration Tests', function () {
   });  
 
   it('emits "play" messages to clients, which contain a URL (string), title (string) and play time (integer) for each Youtube video', function (done) {
-    app.queueSong('https://www.youtube.com/watch?v=3PEGDGxZdzA');
-
     socket.on('play', function (data) {
       expect(data).to.contain.all.keys(['url', 'title', 'time']);
       expect(typeof data.url).to.equal('string');
@@ -60,19 +60,23 @@ describe('glitch Server Integration Tests', function () {
   });
 
   it('gives clients who connect after a video has started playing a non-zero play time which is equal to the server play time', function (done) {
-    socket.on('play', function (data) {
-      expect(data.time).to.be.above(0); //Just checks to see that the play time provided is greater than zero, as the server has already been running for some time
-      socket.disconnect();
-      done();
-    });
+    setTimeout(function() {
+      var socket2 = io.connect("http://localhost:" + socketPort, {
+        transports: ['websocket'],
+        'force new connection': true
+      });
+      socket2.on('play', function (data) {
+        expect(data.time).to.be.above(0); //Just checks to see that the play time provided is greater than zero, as the server has already been running for some time
+        socket2.disconnect();
+        done();
+      });
+    }, 1000);
   });
 
   it('records the time at which the video was started and calculates the time at which playback of the video should end', function (done) {
     socket.on('play', function (data) {
       var currentSong = app.getCurrentSong();
-      expect(moment().diff(currentSong.startMoment)).to.be.above(0);  //Checks to see that the recorded start time is earlier than the current time
-      expect(currentSong.endMoment.diff(moment())).to.be.above(0);  //Checks to see that the calculated end time is later than the current time
-      socket.disconnect();
+      expect(currentSong.endMoment.diff(currentSong.startMoment)).to.be.above(0);  //Checks to see that there is a calculated end time which is after the start time
       done();
     });
   });

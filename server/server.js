66ed12fff72f64a862c193f652bfdebd790ed501
Yilin Xu <yx296@nyu.edu>
2015-09-30 21:58:12 -0700

    Attempt to fix playlist

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ var activeSockets = [];
 var numActiveClients = 0;
 var chatMessages = [];
 var lastChatIdx = -1;
-var currentPlaylist;
+var currentPlaylist = [];
 
 // Configuration variables
 var chatAnalysisTime = 5000;
@@ -38,14 +38,21 @@ var chatAnalysisTime = 5000;
 var currentSong = module.exports.currentSong = {startMoment: null, endMoment: null, title: null};
 
 //Read playlist file, parses playlist into an array and run server with the playlist array
-var fetchPlaylistFromFile = function (callback) {
-  fs.readFile(__dirname + '/playlist.json', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    var playlist = JSON.parse(data);
-    callback(playlist);
-  });
+
+// var fetchPlaylistFromFile = function (callback) {
+//   fs.readFile(__dirname + '/playlist.json', function read(err, data) {
+//     if (err) {
+//       throw err;
+//     }
+//     var playlist = JSON.parse(data);
+//     callback(playlist);
+//   });
+// };
+
+var startServer = function() {
+  setUpSockets();  
+  handlePlaylist();
+  analyzeChat();
 };
 
 var fetchPlaylistFromYouTube = function (queryString, callback) {
@@ -102,28 +109,27 @@ var setUpSockets = function () {
   console.log('sockets established...');
 };
 
-var handlePlaylist = function (playlist) {
-  currentPlaylist = playlist; //Creates a copy of the playlist; entries will be deleted from this copy as they are played
-  
+
+var handlePlaylist = function () {
   var donePlaying = true;
-  var intervalId; 
+  
+  setInterval(function() {
+    if(donePlaying && currentPlaylist.length > 0) {
+      playSong(currentPlaylist[0]); //Updates the currentSong object with the first song in the playlist
+      donePlaying = false;
+    }         
 
-  if(playlist.length > 0) { // if a playlist with songs in it was passed in
+    
+    if(moment().isAfter(currentSong.endMoment)) {  //If the current time is after the endTime for the current entry being played
+      donePlaying = true;
+      currentPlaylist.shift();  //Deletes an entry from the playlist after it is done playing
+      console.log(currentPlaylist);
+    }
 
-    intervalId = setInterval(function() {
-      if(moment().isAfter(currentSong.endMoment)) {  //If the current time is after the endTime for the current entry being played
-        donePlaying = true;
-        currentPlaylist.shift();  //Deletes an entry from the playlist after it is done playing
-      }
-
-      //Plays the first element from the playlist if the current song is done playing and the playlist is not empty
-      if(donePlaying && currentPlaylist.length > 0) {
-        playSong(currentPlaylist[0]); //Updates the currentSong object with the first song in the playlist
-        donePlaying = false;
-      }                                      
-    }, 1000);
-  }
+    //Plays the first element from the playlist if the current song is done playing and the playlist is not empty
+  }, 1000);
 };
+
 
 function analyzeChat() {
   setInterval(function() {
@@ -137,10 +143,11 @@ function analyzeChat() {
     }
 
     // For each bang, use the Youtube Search API
-    for (var i = 0; i < bangs.length; i++) {
-      fetchPlaylistFromYouTube(bangs[i], function(results) {
+    for (var j = 0; j < bangs.length; j++) {
+      fetchPlaylistFromYouTube(bangs[j], function(results) {
         // Add the top result to our playlist
         currentPlaylist.push(results[0]);
+        console.log(currentPlaylist);
       });
     }
   }, chatAnalysisTime);
@@ -183,11 +190,14 @@ var playSong = function(playlistEntry) {
 
 
 // Start running the server
-fetchPlaylistFromFile(function (playlist) {
-  setUpSockets();  
-  handlePlaylist(playlist);
-  analyzeChat();
-});
+
+startServer();
+
+// fetchPlaylistFromFile(function (playlist) {
+  // setUpSockets();  
+  // handlePlaylist(playlist);
+  // analyzeChat();
+// });
 
 //The exported functions below are currently used for testing;  they can be safely deleted (or removed from export) at deployment
 

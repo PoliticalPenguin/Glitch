67a04a9b612ceeedc2b7f0aa3e6e01e2901098c7
Nate Willard <nate.a.willard@gmail.com>
2015-10-01 09:31:51 -0700

    Refactor playlist handler into separate module

---
+++ server/app/playlistHandler.js
@@ -0,0 +1,64 @@
+var https = require('https');
+var moment = require('moment');
+var app = require(__dirname+'../../server.js');
+var socketHandler = require(__dirname+'/socketHandler.js');
+var youtubeKey = app.youtubeKey;
+
+// Object which represents the current song being played; stores song title, start moment at which server told clients to first play the song, and end moment at which playback should end  
+module.exports.currentSong = {startMoment: null, endMoment: null, title: null};
+
+module.exports.handlePlaylist = function () {
+  var donePlaying = true;
+  
+  setInterval(function() {
+    var currentPlaylist = app.getPlaylist();
+    if(donePlaying && currentPlaylist.length > 0) {
+      playSong(currentPlaylist[0]); //Updates the currentSong object with the first song in the playlist
+      donePlaying = false;
+      module.exports.currentSong = currentPlaylist[0];
+    }         
+
+    
+    if(moment().isAfter(module.exports.currentSong.endMoment)) {  //If the current time is after the endTime for the current entry being played
+      donePlaying = true;
+      currentPlaylist.shift();  //Deletes an entry from the playlist after it is done playing
+    }
+
+    //Plays the first element from the playlist if the current song is done playing and the playlist is not empty
+  }, 1000);
+};
+
+var playSong = function(playlistEntry) {
+  var parsedEntry = playlistEntry.split('=');
+  
+  var requestString = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=' + parsedEntry[1] + '&key=' + youtubeKey; 
+
+  https.get(requestString, function(res) {
+    var body = '';
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      var object = JSON.parse(body);
+      var contentDetails = object.items[0].contentDetails;
+      var snippet = object.items[0].snippet;
+      
+      var videoDuration = moment.duration(contentDetails.duration);
+
+      var end = moment();
+      end.add(videoDuration);
+
+      var newSong = {};
+      newSong.id = parsedEntry[1];
+      newSong.url = playlistEntry;
+      newSong.title = snippet.title;
+      newSong.startMoment = moment();
+      newSong.endMoment = end;
+      console.log(newSong.title + ' is now playing.  Video will end ' + newSong.endMoment.calendar());
+      module.exports.currentSong = newSong;
+      socketHandler.io.emit('play', {url: newSong.url, title: module.exports.currentSong.title, time: 0});
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  }); 
+};

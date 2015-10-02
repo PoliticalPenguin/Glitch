4c69c547139067d0b0cc1fa96b8f7ebccddd68c2
Nate Willard <nate.a.willard@gmail.com>
2015-10-01 21:03:00 -0700

    lint client angular tests

--- test/client/angular.js
+++ test/client/angular.js
@@ -2,53 +2,67 @@ var assert = chai.assert;
 var should = chai.should();
 var expect = chai.expect;
 
-describe('Client-side Angular', function() {
+describe('Client-side Angular', function () {
   beforeEach(module('glitch'));
 
   var $controller;
   var socket;
 
-  beforeEach(inject(function(_$controller_, _socket_) {
+  beforeEach(inject(function (_$controller_, _socket_) {
     $controller = _$controller_;
     socket = _socket_;
   }));
 
-  describe('YouTube Handling', function() {
+  describe('YouTube Handling', function () {
     var $scope;
     var controller;
-    beforeEach(function() {
+    beforeEach(function () {
       $scope = {};
-      controller = $controller('youtubeController', {$scope: $scope});
+      controller = $controller('youtubeController', {
+        $scope: $scope
+      });
     });
 
-    describe('Video Display', function() {
-      it('should have a placeholder initial video', function() {
+    describe('Video Display', function () {
+      it('should have a placeholder initial video', function () {
         expect($scope.currentVideo).to.exist;
         expect($scope.currentVideo.title).to.equal('Waiting For Server...');
       });
-      it('should display videos', function(done) {
-        socket.emit('echo', {name: 'play', data: {url: 'https://www.youtube.com/watch?v=3PEGDGxZdzA', title: "Emancipator - Anthem (2006)"}});
+      it('should display videos', function (done) {
+        socket.emit('echo', {
+          name: 'play',
+          data: {
+            url: 'https://www.youtube.com/watch?v=3PEGDGxZdzA',
+              title: "Emancipator - Anthem (2006)"
+          }
+        });
         this.timeout(3000);
-        setTimeout(function() {
+        setTimeout(function () {
           expect($scope.currentVideo.title).to.not.equal('Waiting For Server...');
           done();
         }, 2000);
       });
     });
-    describe('Song History', function() {
-      it('Should have an empty initial playlist', function() {
+    describe('Song History', function () {
+      it('Should have an empty initial playlist', function () {
         expect($scope.pastVideos).to.have.length(0);
       });
-      it('should add videos to the playlist', function(done) {
-        socket.emit('echo', {name: 'play', data: {url: 'https://www.youtube.com/watch?v=3PEGDGxZdzA', title: "Emancipator - Anthem (2006)"}});
+      it('should add videos to the playlist', function (done) {
+        socket.emit('echo', {
+          name: 'play',
+          data: {
+            url: 'https://www.youtube.com/watch?v=3PEGDGxZdzA',
+              title: "Emancipator - Anthem (2006)"
+          }
+        });
         this.timeout(3000);
-        setTimeout(function() {
+        setTimeout(function () {
           expect($scope.pastVideos.length).to.not.equal(0);
           done();
         }, 2000);
       });
     });
   });
-  describe('Chat Message Handling', function() {
+  describe('Chat Message Handling', function () {
   });
 });

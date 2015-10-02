fba6c8e4a91877680b600df27a678177fa51e693
Nate Willard <nate.a.willard@gmail.com>
2015-10-01 20:45:31 -0700

    Cleanup playlist handler to follow lint

--- server/app/playlistHandler.js
+++ server/app/playlistHandler.js
@@ -1,24 +1,28 @@
 var moment = require('moment');
-var app = require(__dirname+'/../server.js');
-var socketHandler = require(__dirname+'/socketHandler.js');
-var youtube = require(__dirname+'/youtubeUtilities.js');
+var app = require(__dirname + '/../server.js');
+var socketHandler = require(__dirname + '/socketHandler.js');
+var youtube = require(__dirname + '/youtubeUtilities.js');
 
-// Object which represents the current song being played; stores song title, start moment at which server told clients to first play the song, and end moment at which playback should end  
-module.exports.currentSong = {startMoment: null, endMoment: null, title: null};
+// Object which represents the current song being played; stores song title, start moment at which server told clients to first play the song, and end moment at which playback should end
+module.exports.currentSong = {
+  startMoment: null, 
+  endMoment: null, 
+  title: null
+};
 
 module.exports.handlePlaylist = function () {
   var donePlaying = true;
-  
-  setInterval(function() {
+
+  setInterval(function () {
     var currentPlaylist = app.getPlaylist();
-    if(donePlaying && currentPlaylist.length > 0) {
+    if (donePlaying && currentPlaylist.length > 0) {
       playSong(currentPlaylist[0]); //Updates the currentSong object with the first song in the playlist
       donePlaying = false;
       module.exports.currentSong = currentPlaylist[0];
-    }         
+    }
 
     
-    if(moment().isAfter(module.exports.currentSong.endMoment)) {  //If the current time is after the endTime for the current entry being played
+    if (moment().isAfter(module.exports.currentSong.endMoment)) {  //If the current time is after the endTime for the current entry being played
       donePlaying = true;
       currentPlaylist.shift();  //Deletes an entry from the playlist after it is done playing
     }
@@ -27,9 +31,9 @@ module.exports.handlePlaylist = function () {
   }, app.playlistAnalysisTime);
 };
 
-var playSong = module.exports.playSong = function(playlistEntry) {
+var playSong = module.exports.playSong = function (playlistEntry) {
   var parsedEntry = playlistEntry.split('=');
-  youtube.getSongInfo(parsedEntry, function(object) {
+  youtube.getSongInfo(parsedEntry, function (object) {
     var contentDetails = object.items[0].contentDetails;
     var snippet = object.items[0].snippet;
     
